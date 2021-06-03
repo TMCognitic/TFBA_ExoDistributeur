adf6b6354cc2cf28f420e56f23169d7b6821fc2b
Morre Thierry <Briareos@live.be>
2021-06-03 15:50:19 +0200

    Add project files.

---
+++ ExoDistributeur/Models/Distributeur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoDistributeur.Models
+{
+    struct Distributeur
+    {
+        public Boisson Get()
+        {
+            Boisson b = new Boisson();
+            b.Nom = "Eau";
+
+            return b;
+        }
+    }
+}
---
+++ ExoDistributeur/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExoDistributeur.Models;
+using System;
+
+namespace ExoDistributeur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distributeur distributeur = new Distributeur();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Boisson boisson = distributeur.Get();
+                Console.WriteLine(boisson.Nom);
+            }
+        }
+    }
+}
